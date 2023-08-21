b4d504cfae43257f7be5be09542fe40301672efa
Akash Gautam <76935019+geekblower@users.noreply.github.com>
2023-08-22 00:02:07 +0530

    Leetcode

--- Day 11-20/Find the Longest Equal Subarray.cpp
+++ Day 11-20/Find the Longest Equal Subarray.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int longestEqualSubarray(vector<int>& nums, int k) {
         unordered_map<int,int> map;
-        int maxCount =0 ;
-        int back = 0 ;
-        int ans =0 ;
+        int maxCount = 0;
+        int back = 0;
+        int ans = 0;
         
-        for(int r=0; r<nums.size(); r++){
-            if(map.find(nums[r]) == map.end()){
-                map[nums[r]] = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(map.find(nums[i]) == map.end()){
+                map[nums[i]] = 0;
             }
 
             // adding into map
-            map[nums[r]]++;
+            map[nums[i]]++;
 
             // count max freq
-            if(map[nums[r]] > maxCount) maxCount = map[nums[r]];
+            if(map[nums[i]] > maxCount) maxCount = map[nums[i]];
 
-            if((r - back+1 - maxCount) > k){
+            if((i - back+1 - maxCount) > k){
                 map[nums[back]] -= 1;
                 back++;
             }
